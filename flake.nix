2fcb048bb42f1225f9a4448503c518d9fcb49c09
zincentimeter <zincentimeter@outlook.com>
2025-04-02 00:06:45 +0800

    ci: prepare for cachix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My personal NUR repository";
+  description = "Shinri's personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
